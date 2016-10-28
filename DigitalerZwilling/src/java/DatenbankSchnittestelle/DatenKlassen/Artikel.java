5f0c4604ef5016b28de553cd64d43168bb789371
iPorre <florian.kruckmann@hs-osnabrueck.de>
2016-10-28 12:12:42 +0200

    alter stand

--- DigitalerZwilling/src/java/DatenbankSchnittestelle/DatenKlassen/Artikel.java
+++ DigitalerZwilling/src/java/DatenbankSchnittestelle/DatenKlassen/Artikel.java
@@ -7,32 +7,47 @@ package DatenbankSchnittestelle.DatenKlassen;
 
 import DatenbankSchnittestelle.DatenSchnittstelle;
 import DatenbankSchnittestelle.Qualifier.db;
+import java.io.Serializable;
 import java.sql.Timestamp;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
+import javax.enterprise.inject.Any;
 import javax.inject.Inject;
 
 /**
  *
  * @author chris
  */
-public class Artikel {
-    
-    @Inject @db private DatenSchnittstelle data;
-    private long id_Artikel;
-    private String bezeichnung;
-    private Timestamp zeit;
-    private String userparameter;
-    //private List<Warentraeger> liegt_auf_Warentraeger;
 
-    public Artikel(long id_Artikel, String bezeichnung, Timestamp zeit, String userparameter) {
+
+public class Artikel{
+    
+    private final DatenSchnittstelle data;
+    private final long id_Artikel;
+    private final String bezeichnung;
+    private final Timestamp zeit;
+    private final String user_Parameter;
+    
+
+    public Artikel(DatenSchnittstelle data, long id_Artikel, String bezeichnung, Timestamp zeit, String user_Parameter) {
+        this.data = data;
         this.id_Artikel = id_Artikel;
         this.bezeichnung = bezeichnung;
-        this.zeit = zeit;  
-        this.userparameter = userparameter;
+        this.zeit = zeit;
+        this.user_Parameter = user_Parameter;
     }
+
+    
+    
+    @PostConstruct
+    public void init(){
+        System.out.println("hmm");
+        System.out.println(this.data.toString());
+    }
+
+    
 
     public long getId_Artikel() {
         return id_Artikel;
@@ -47,13 +62,18 @@ public class Artikel {
     }
 
     public String getUserparameter() {
-        return userparameter;
+        return user_Parameter;
     }
 
+    /**
+     *
+     * @return Gibt Warentraeger zurueck
+     * @throws Exception
+     */
     public List<Warentraeger> getLiegt_auf_Warentraeger() throws Exception {
         return data._Artikel_Warentraeger(id_Artikel);
     }
-    
+   
     
     
 }
--- DigitalerZwilling/src/java/DatenbankSchnittestelle/DatenKlassen/Gelenk.java
+++ DigitalerZwilling/src/java/DatenbankSchnittestelle/DatenKlassen/Gelenk.java
@@ -5,10 +5,60 @@
  */
 package DatenbankSchnittestelle.DatenKlassen;
 
+import DatenbankSchnittestelle.DatenSchnittstelle;
+import DatenbankSchnittestelle.Qualifier.db;
+import java.sql.Timestamp;
+import javax.inject.Inject;
+
 /**
  *
  * @author chris
  */
 public class Gelenk {
+    private final DatenSchnittstelle data;
+    private final long id_Gelenk;
+    private final String gelenktyp;           //hmmm
+    private final int nummer;
+    private final int gelenkstellung;          // evtl. auch mehere Variabeln
+    private final Timestamp zeitstempel;
+    private final String user_Paameter;
+
+    public Gelenk(DatenSchnittstelle data, long id_Gelenk, String gelenktyp, int nummer, int gelenkstellung, Timestamp zeitstempel, String user_Paameter) {
+        this.data = data;
+        this.id_Gelenk = id_Gelenk;
+        this.gelenktyp = gelenktyp;
+        this.nummer = nummer;
+        this.gelenkstellung = gelenkstellung;
+        this.zeitstempel = zeitstempel;
+        this.user_Paameter = user_Paameter;
+    }
+
+    public long getId_Gelenk() {
+        return id_Gelenk;
+    }
+
+    public String getGelenktyp() {
+        return gelenktyp;
+    }
+
+    public int getNummer() {
+        return nummer;
+    }
+
+    public int getGelenkstellung() {
+        return gelenkstellung;
+    }
+
+    public Timestamp getZeitstempel() {
+        return zeitstempel;
+    }
+
+    public String getUser_Paameter() {
+        return user_Paameter;
+    }
     
+    public Roboter getRoboter(){
+        return this.data._Gelenk_Roboter(id_Gelenk);
+    }
+   
 }
